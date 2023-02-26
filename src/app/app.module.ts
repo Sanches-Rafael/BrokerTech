ac8dbbe45665ffa5a74cb42f1800dc1fff5917bc
SanchesRafael <myself_sanchesrafael@outlook.com>
2023-02-26 01:43:27 -0300

    Atual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddContentComponent } from './view/add-content/add-content.component';
+import { ViewComponent } from './view/view.component';
+
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewComponent,
+    AddContentComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
